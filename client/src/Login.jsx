9f63939e24ec80a9ec47a0ee2f58f59b68e9ec9b
Yogesh Kumar <kumaryogesh8490@gmail.com>
2023-06-17 23:09:24 +0530

    changed className in login

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -36,8 +36,8 @@ export default class Login extends React.Component {
                     <div className="field">
                         <input type="password" name="password" value={this.state.password} onChange={this.passwordChangeHandler} placeholder="Password" />
                     </div>
-                    <div class="field">
-                        <div class="ui checkbox">
+                    <div className="field">
+                        <div className="ui checkbox">
                             <input type="checkbox" name="rememberMe" isChecked={this.state.rememberMeCheck} onChange={this.rememberMeCheckHandler} />
                             <label>Remember Me</label>
                         </div>
